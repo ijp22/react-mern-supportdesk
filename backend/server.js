15d039bcbafb05acdfbb01539b71d62e3a2f99c8
Ian Petts <ianpetts@Ians-MacBook-Pro.local>
2022-02-23 17:42:40 +0000

    v2

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
@@ -14,12 +14,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to the MERN Support Desk API' });
-});
-
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  // Static Folder
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the MERN Support Desk API' });
+  });
+}
 
 app.use(errorHandler);
 
